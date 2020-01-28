4cfbbb859bb39a6d95788763ded8690bec73bc20
Alexander Terry <alexterry43@gmail.com>
2020-01-28 15:09:17 -0700

    bonus

--- app/bonuses.js
+++ app/bonuses.js
@@ -2,5 +2,24 @@
 let str = "team number 2"
 
 function camelCase(str) {
+  let ret = "";
+  let flag;
+  for (let i = 0; i < str.length; i++) {
+    let letter = str[i]
+    if (letter == ' ') {
+      flag = true;
+      continue;
+    }
+    if (flag) {
+      ret += letter.toUpperCase();
+      flag = false;
+    } else {
+      ret += letter;
+    }
+
+  }
+
+  return ret;
+
 
 }
